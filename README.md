6daa0ceda4b02deaaef4d0ecb8f8559d033f1630
kidoda <kdevb0x@gmail.com>
2018-02-03 05:24:22 -0700

    changed README and added little more code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# quickcam: A quick and dirty program to snap an image with a webcam on linux.
+<h1> #quikcam: </h1>
+<p> A quick and dirty program to snap an image with a webcam on linux. </p>
 
--- quikcam.go
+++ quikcam.go
@@ -1,28 +1,48 @@
 package main
 
 import (
+	"log"
 	"os"
-	"bytes"
-	"io"
-	"bufio"
-	"io/ioutil"
+
+	"golang.org/x/sys/unix"
+	//"github.com/blackjack/webcam/ioctl"
 )
 
-func newCam(path string) webcam, error {
-	dev := os.Open(path)
-
-	cam.
+// Open webcam device
+func newCam(path string) (*Webcam, error) {
+	dev, err := os.OpenFile(path, unix.O_RDWR|unix.O_NONBLOCK, 0)
+	fd := dev.Fd()
+	if err != nil || fd < 0 {
+		log.Fatal("There was an error opening the device: %s", err)
+		return nil, err
+	}
+	cam := new(Webcam)
+	cam.devpath = dev
+	cam.fd = fd
+	return cam, nil
+	//buff := make([][]byte, dev)
 
 }
-type Webcam interface {
-	Scan
-
+func (w *Webcam) GrabFrame() error {
+	return nil
 }
-type webcam struct {
-	devpath string
+
+type Cam interface {
+	GrabFrame() error
+}
+type Webcam struct {
+	devpath *os.File
+	fd      uintptr
+	buffers [][]byte
 }
 
 func main() {
 	var path = "/dev/video0"
-	cam := newCam(path)
+	webcam, err := newCam(path)
+	if err != nil {
+		log.Println("There was an error instantiating webcam: %s", err)
+	}
+	stub := webcam.GrabFrame()
+	log.Println(stub)
+	os.Exit(0)
 }
